51e953b1ab73d953a576301363ca3933df9ce1a4
mmNihal <xxxxmd309@gmail.com>
2025-06-27 02:35:57 +0600

    file name mistake

--- src/provider/index.js
+++ src/provider/index.js
@@ -1,3 +1,3 @@
 export { default as FavoriteProvider } from "./FavoriteProvider";
 export { default as LocationsProvider } from "./LocationProvider";
-export { default as WeatherProvider } from "./WeatherProvider";
+export { default as WeatherProvider } from "./weatherProvider";
